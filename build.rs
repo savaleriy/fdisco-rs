bcb025a66c67e3fe10d245aeea3810fde5272616
savaleriy <valera.semenyuk15@gmail.com>
2025-07-21 17:11:19 +0700

    remove slint staff

--- build.rs
+++ build.rs
@@ -1,11 +1,4 @@
 fn main() {
-    #[cfg(feature = "starter")]
-    slint_build::compile_with_config(
-        "src/bin/app-window.slint",
-        slint_build::CompilerConfiguration::new()
-            .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
-    )
-    .unwrap();
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
